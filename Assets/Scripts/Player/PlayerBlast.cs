3f6531dcd825374f18d28ca6838b255fc4122067
jackamirebrahimi <jackamirebrahimi@gmail.com>
2026-05-06 19:41:33 +0100

    Added holding down the buttons for charged blasts

--- Assets/Scripts/Player/PlayerBlast.cs
+++ Assets/Scripts/Player/PlayerBlast.cs
@@ -7,18 +7,51 @@ public class PlayerBlast : MonoBehaviour
     private Rigidbody2D rb;
     private PlayerStats _playerStats;
 
+    //Timers
+    [SerializeField] private float holdBlastTimer;
+    private float[] _blastTimers; //0 - Down, 1 - Left, 2 - Right, 3 - Up
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         _playerStats = GetComponent<PlayerStats>();
+
+        _blastTimers = new float[4];
+    }
+
+    private void Update()
+    {
+        //Checks for timers
+        for (int i = 0; i < _blastTimers.Length; i++)
+        {
+            if (_blastTimers[i] >= 0)
+            {
+                _blastTimers[i] += Time.deltaTime;
+            }
+        }
     }
 
     public void BlastDown(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            rb.linearVelocity = new Vector2 (rb.linearVelocity.x, 0f);
-            rb.AddForce(new Vector2(0f, 100f * 4), ForceMode2D.Force);
+            _blastTimers[0] = 0f;
+        }
+
+        if (context.canceled)
+        {
+            if (_blastTimers[0] > holdBlastTimer) //Blast Jump
+            {
+                rb.linearVelocity = Vector2.zero;
+                rb.AddForce(new Vector2(0f, 100f * 7), ForceMode2D.Force);
+            }
+            else //Normal Jump
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+                rb.AddForce(new Vector2(0f, 100f * 4), ForceMode2D.Force);
+            }
+
+            _blastTimers[0] = -1f;
         }
     }
 
@@ -26,7 +59,23 @@ public class PlayerBlast : MonoBehaviour
     {
         if (context.performed)
         {
+            _blastTimers[1] = 0f;
             rb.AddForce(new Vector2(100f * 4, 0f), ForceMode2D.Force);
+        }
+
+        if (context.canceled)
+        {
+            if (_blastTimers[1] > holdBlastTimer) //Blast Jump
+            {
+                rb.linearVelocity = Vector2.zero;
+                rb.AddForce(new Vector2(100f * 7, 0f), ForceMode2D.Force);
+            }
+            else //Normal Jump
+            {
+                rb.AddForce(new Vector2(100f * 4, 0f), ForceMode2D.Force);
+            }
+
+            _blastTimers[1] = -1f;
         }
     }
 
@@ -34,7 +83,23 @@ public class PlayerBlast : MonoBehaviour
     {
         if (context.performed)
         {
+            _blastTimers[2] = 0f;
             rb.AddForce(new Vector2(100f * -4, 0f), ForceMode2D.Force);
+        }
+
+        if (context.canceled)
+        {
+            if (_blastTimers[2] > holdBlastTimer) //Blast Jump
+            {
+                rb.linearVelocity = Vector2.zero;
+                rb.AddForce(new Vector2(100f * -7, 0f), ForceMode2D.Force);
+            }
+            else //Normal Jump
+            {
+                rb.AddForce(new Vector2(100f * -4, 0f), ForceMode2D.Force);
+            }
+
+            _blastTimers[2] = -1f;
         }
     }
 
@@ -42,7 +107,22 @@ public class PlayerBlast : MonoBehaviour
     {
         if (context.performed)
         {
-            rb.AddForce(new Vector2(0f, 100f * -4), ForceMode2D.Force);
+            _blastTimers[3] = 0f;
+        }
+
+        if (context.canceled)
+        {
+            if (_blastTimers[3] > holdBlastTimer) //Blast Jump
+            {
+                rb.linearVelocity = Vector2.zero;
+                rb.AddForce(new Vector2(0f, 100f * -7), ForceMode2D.Force);
+            }
+            else //Normal Jump
+            {
+                rb.AddForce(new Vector2(0f, 100f * -4), ForceMode2D.Force);
+            }
+
+            _blastTimers[3] = -1f;
         }
     }
 }
